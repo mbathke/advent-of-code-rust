f4181e5d7dcc673d3a801623d8dc04ecbd288a3b
Michael Bathke <mbathke666@gmail.com>
2024-12-04 22:34:03 +0100

    feat: added nom parsing lib for day 3

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,10 +1,33 @@
+use nom::{bytes::{complete::take_while_m_n, streaming::is_a}, combinator::map_res, sequence::tuple, IResult};
+
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+#[derive(Debug, PartialEq)]
+pub struct PuzzleResult(u32);
+
+fn from_dec(input: &str) -> Result<u32, std::num::ParseIntError> {
+  u32::from_str_radix(input, 10)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+fn is_digit(c: char) -> bool {
+  c.is_digit(16)
+}
+
+fn primary(input: &str) -> IResult<&str, u32> {
+  map_res(
+    take_while_m_n(2, 2, is_digit),
+    from_dec
+  )(input)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (input, _) = is_a::<&str, &str, nom::error::Error<&str>>("mul")(input).unwrap();
+    let (_input, (mul_1, mul_2)) = tuple((primary, primary))(input).unwrap();
+    let multiplicator = mul_1 * mul_2;
+    Some(multiplicator)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -14,8 +37,9 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let input = advent_of_code::template::read_file("examples", DAY);
+        let result = part_one(&input);
+        assert_eq!(result, Some(161));
     }
 
     #[test]
