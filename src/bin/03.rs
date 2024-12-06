79d4e1f765f81d5427c8d089f9fd0fd95d45f2d6
Michael Bathke <mbathke666@gmail.com>
2024-12-06 18:47:06 +0100

    feat: replaced nom with pest

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,30 +1,13 @@
-use nom::{bytes::{complete::take_while_m_n, streaming::is_a}, combinator::map_res, sequence::tuple, IResult};
+use pest::Parser;
 
 advent_of_code::solution!(3);
 
-#[derive(Debug, PartialEq)]
-pub struct PuzzleResult(u32);
-
-fn from_dec(input: &str) -> Result<u32, std::num::ParseIntError> {
-  u32::from_str_radix(input, 10)
-}
-
-fn is_digit(c: char) -> bool {
-  c.is_digit(16)
-}
-
-fn primary(input: &str) -> IResult<&str, u32> {
-  map_res(
-    take_while_m_n(2, 2, is_digit),
-    from_dec
-  )(input)
-}
+#[derive(Parser)]
+#[grammar = "../pest/03.pest"]
+struct PestParser;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (input, _) = is_a::<&str, &str, nom::error::Error<&str>>("mul")(input).unwrap();
-    let (_input, (mul_1, mul_2)) = tuple((primary, primary))(input).unwrap();
-    let multiplicator = mul_1 * mul_2;
-    Some(multiplicator)
+    None
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
