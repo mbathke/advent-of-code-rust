e122c3617efc41fe4f9f10ec647a8f6e80025deb
Michael Bathke <mbathke666@gmail.com>
2024-12-10 21:37:11 +0100

    feat: try to find my way to part two of day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -6,13 +6,13 @@ pub fn part_one(input: &str) -> Option<i32> {
     let mut matrix: Vec<Vec<char>> = Vec::new();
     let search_term: Vec<char> = vec!['X', 'M', 'A', 'S'];
 
-    println!("[");
+    // println!("[");
     for line in input.lines() {
         let line_chars = line.chars().collect();
-        println!("{:?}", line_chars);
+        // println!("{:?}", line_chars);
         matrix.push(line_chars);
     }
-    println!("]");
+    // println!("]");
 
     let mut found_xmas = 0;
 
@@ -24,7 +24,7 @@ pub fn part_one(input: &str) -> Option<i32> {
                         if dy == 0 && dx == 0 {
                             continue;
                         }
-                        let mut all_ok = true;
+                        let mut keyword_matches = true;
                         for (i, &val) in search_term.iter().enumerate() {
                             let row = y as i32 + dy * i as i32;
                             let col = x as i32 + dx * i as i32;
@@ -35,12 +35,12 @@ pub fn part_one(input: &str) -> Option<i32> {
                                 || (col as usize) >= matrix[y].len()
                                 || matrix[row as usize][col as usize] != val
                             {
-                                all_ok = false;
+                                keyword_matches = false;
                                 break;
                             }
                         }
 
-                        if all_ok {
+                        if keyword_matches {
                             found_xmas += 1
                         }
                     }
@@ -53,7 +53,54 @@ pub fn part_one(input: &str) -> Option<i32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    let search_term: Vec<char> = vec!['M', 'A', 'S'];
+
+    // println!("[");
+    for line in input.lines() {
+        let line_chars = line.chars().collect();
+        // println!("{:?}", line_chars);
+        matrix.push(line_chars);
+    }
+    // println!("]");
+
+    let mut found_xmas = 0;
+
+    for y in 0..matrix.len() {
+        for x in 0..matrix[y].len() {
+            if matrix[y][x] == 'M' {
+                for dy in -1..=1 as i32 {
+                    for dx in -1..=1 as i32 {
+                        if dy == 0 || dx == 0 {
+                            continue;
+                        }
+
+                        let mut keyword_matches = true;
+                        for (i, &val) in search_term.iter().enumerate() {
+                            let row = y as i32 + dy * i as i32;
+                            let col = x as i32 + dx * i as i32;
+
+                            if 0 > row
+                                || (row as usize) >= matrix.len()
+                                || 0 > col
+                                || (col as usize) >= matrix[y].len()
+                                || matrix[row as usize][col as usize] != val
+                            {
+                                keyword_matches = false;
+                                break;
+                            }
+                        }
+
+                        if keyword_matches {
+                            found_xmas += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Some(found_xmas)
 }
 
 #[cfg(test)]
@@ -69,6 +116,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
